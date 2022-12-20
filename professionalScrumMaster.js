289b4e307f991c76e504a8d3af3fc7e96307ebf0
progvocab <progvocab@gmail.com>
2022-12-20 21:56:29 +0530

    tweek

---
+++ professionalScrumMaster.js
@@ -0,0 +1,15 @@
+
+var req = [
+    {
+                    'question': `who defines sprint goal?`
+                    , 'optiona': ' scrum team',
+                    'optionb': ' product owner',
+                    'optionc': ' scrum mastery',
+                    'optiond': 'developers',
+                    'answer': 'a',
+                    'info': ' <a href="https://scrumguides.org/scrum-guide.html#sprint-planning" target="blank" > link </a>  The whole Scrum Team then collaborates to define a Sprint Goal that communicates why the Sprint is valuable to stakeholders.   '
+
+                }
+];
+
+var title = "Core Values"
